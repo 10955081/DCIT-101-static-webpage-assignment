439a97e8a1b8075bc46dd2f6e135492dfae0f25d
10955081 <102700165+10955081@users.noreply.github.com>
2022-04-20 16:59:56 +0000

    Add files via upload

---
+++ index.js
@@ -0,0 +1,8 @@
+//Welcomes the user into the webpage
+alert("Welcome to 10955081's webpage");
+
+//Alert the user that information sent is sucessful
+function sent() {
+    alert("Sent successfully");
+}
+
